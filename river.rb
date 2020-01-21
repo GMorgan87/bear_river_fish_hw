cf1e76235a81443168296f43a8a616a8361d79db
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-01-21 17:55:02 +0000

    adds fish_count and lose_fish to river class

--- river.rb
+++ river.rb
@@ -2,11 +2,19 @@ class River
 
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, start_fish)
     @name = name
-    @fishes = []
+    @fishes = start_fish
+    #bad english but makes clear that @fishes is an array and not an individual fish
   end
 
+  def fish_count
+    return @fishes.length()
+  end
+
+  def lose_fish
+    return @fishes.pop()
+  end
 
 
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -3,15 +3,40 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../river')
+require_relative('../fish')
 
 class TestRiver < Minitest::Test
 
   def setup
-    @river1 = River.new("Forth")
+    @fish1 = Fish.new("Nemo")
+    @fish2 = Fish.new("Dory")
+    @fish3 = Fish.new("Billy Bass")
+
+    @start_fish = [@fish1, @fish2, @fish3]
+
+    @river1 = River.new("Forth", @start_fish)
   end
 
   def test_get_name
     assert_equal("Forth", @river1.name)
   end
+
+  def test_fish_count
+    assert_equal(3, @river1.fish_count)
+  end
+
+  def test_lose_fish
+    taken_fish = @river1.lose_fish
+    assert_equal("Billy Bass", taken_fish.name)
+    assert_equal(2, @river1.fish_count)
+  end
+
+
+
+
+  # def test_add_fish
+  #   @river1.add_fish(@new_fishes)
+  #   assert_equal(3, @river1.fish_count)
+  # end
 
 end
